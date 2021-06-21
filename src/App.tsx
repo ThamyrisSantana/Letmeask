c8d8134656d1ed0c4c690224d529bd3a0a6a5dca
Thamyris <thamyrissatana3@gmail.com>
2021-06-21 12:14:25 -0300

    .

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/Button";
+function App() {
+  return (
+    <div>
+      <h1>Hello, Santtt</h1>
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Button() {
+  return (
+    <div>
+      <button>Botão</button>
+    </div>
+  );
+}
